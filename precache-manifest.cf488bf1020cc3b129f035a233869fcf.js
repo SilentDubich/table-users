7c2f6fba48279deef69b857a60ce8a4876667064
SilentDubich <kirill.dubov.2012@mail.ru>
2022-05-04 21:15:02 +0300

    Updates

---
+++ precache-manifest.cf488bf1020cc3b129f035a233869fcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77883a66865328cd4f3d9d6affe2b66b",
+    "url": "/test-fg/index.html"
+  },
+  {
+    "revision": "b81baa89a0d123b06e21",
+    "url": "/test-fg/static/css/main.3826b3aa.chunk.css"
+  },
+  {
+    "revision": "7f7d6ab1819f552d1446",
+    "url": "/test-fg/static/js/2.393da01d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-fg/static/js/2.393da01d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81baa89a0d123b06e21",
+    "url": "/test-fg/static/js/main.513a2c05.chunk.js"
+  },
+  {
+    "revision": "e1532bf22656f3b15022",
+    "url": "/test-fg/static/js/runtime-main.8b29b228.js"
+  },
+  {
+    "revision": "4b7d2a000a8e5960143e77618d1e77d7",
+    "url": "/test-fg/static/media/Spinner-1.4s-3a356a.4b7d2a00.svg"
+  }
+]);
